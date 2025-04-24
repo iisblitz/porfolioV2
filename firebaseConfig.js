26c055e7ac842a1078021f24e15cfbf19a26dbe0
David Gonzalez Alanis <iis_dbga@live.com>
2025-04-24 08:54:03 -0600

    added .env

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,15 +1,15 @@
-// firebaseConfig.js
 import { initializeApp } from "firebase/app";
 import { getFirestore, collection, getDocs } from "firebase/firestore";
+import "dotenv/config";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCBvMG8Ne9hMURKaO6VZnfORy1Mz05VkBs",
-    authDomain: "davidgonzalez-1e347.firebaseapp.com",
-    projectId: "davidgonzalez-1e347",
-    storageBucket: "davidgonzalez-1e347.firebasestorage.app",
-    messagingSenderId: "386061872411",
-    appId: "1:386061872411:web:981e9b239689d7f47237be"
-  };
+  apiKey: APIKEY1,
+  authDomain: ADOMAIN,
+  projectId: PROID,
+  storageBucket: SBUCKET,
+  messagingSenderId: MESSSENDID,
+  appId: APPID1,
+};
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,11 @@
 import { Provider } from "react-redux";
 import store from "@/redux/store";
-
+import "dotenv/config";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <Provider store={store}>
-          {children}
-        </Provider>
+        <Provider store={store}>{children}</Provider>
       </body>
     </html>
   );
